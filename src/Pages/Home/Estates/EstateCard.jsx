2dc910817298c3a4f7e1b09dd041040a1457fab2
Sohelrana2815 <sohel152302@gmail.com>
2024-12-08 11:19:56 +0600

    download customer photo and agent phot

--- src/Pages/Home/Estates/EstateCard.jsx
+++ src/Pages/Home/Estates/EstateCard.jsx
@@ -1,3 +1,6 @@
+import { BiArea, BiLocationPlus } from "react-icons/bi";
+import { GrStatusInfo } from "react-icons/gr";
+
 const EstateCard = ({ estate }) => {
   const {
     id,
@@ -15,16 +18,32 @@ const EstateCard = ({ estate }) => {
   return (
     <>
       <div>
-        <div className="card card-compact bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-            />
+        <div className="card card-compact bg-base-100 shadow-lg">
+          <figure className="md:h-72">
+            <img src={image} alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">{estate_title}</h2>
+            <div className="flex items-center">
+              <p className="flex items-center gap-2 text-base">
+                <GrStatusInfo />
+                For {status}
+              </p>
+              <p className="flex items-center gap-2 text-base">
+                <BiLocationPlus />
+                {location}
+              </p>
+              <p className="flex items-center gap-2 text-base">
+                <BiArea />
+                {area}
+              </p>
+            </div>
+
+            <div className="divider"></div>
+            <div>
+              <img src={agent} alt="" />
+            </div>
+            <p>{price}</p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
             </div>
