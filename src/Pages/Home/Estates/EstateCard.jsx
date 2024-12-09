f9c40f86e2d19dcd2e8ea3d138f82936e8f7ee09
Sohelrana2815 <sohel152302@gmail.com>
2024-12-09 12:04:02 +0600

    Aos fade mode enable

--- src/Pages/Home/Estates/EstateCard.jsx
+++ src/Pages/Home/Estates/EstateCard.jsx
@@ -1,6 +1,7 @@
 import { BiArea, BiLocationPlus } from "react-icons/bi";
 import { GrStatusInfo } from "react-icons/gr";
 import { Link } from "react-router-dom";
+import AnimatedComponent from "../../../Components/AnimatedComponent/AnimatedComponent";
 
 const EstateCard = ({ estate }) => {
   const {
@@ -17,63 +18,65 @@ const EstateCard = ({ estate }) => {
   } = estate;
   return (
     <>
-      <div>
-        <div className="card card-compact bg-base-100 py-2 shadow-md">
-          <figure className="md:h-72 relative xl:border xl:border-blue-500">
-            <p className="absolute text-white rounded-full top-0 left-0 mt-4 ml-2 bg-black bg-opacity-50 px-3 py-1">
-              {segment_name}
-            </p>
-            <img src={image} alt="Shoes" />
-          </figure>
-          <div className="card-body space-y-2">
-            <h2 className="card-title">{estate_title}</h2>
-            <div className="flex items-center">
-              <p className="flex items-center gap-2 text-base">
-                <GrStatusInfo />
-                For {status}
+      <AnimatedComponent animation="fade-in">
+        <div>
+          <div className="card card-compact bg-base-100 py-2 shadow-md">
+            <figure className="md:h-72 relative xl:border xl:border-blue-500">
+              <p className="absolute text-white rounded-full top-0 left-0 mt-4 ml-2 bg-black bg-opacity-50 px-3 py-1">
+                {segment_name}
               </p>
-              <p className="flex items-center gap-2 text-base">
-                <BiLocationPlus />
-                {location}
-              </p>
-              <p className="flex items-center gap-2 text-base">
-                <BiArea />
-                {area}
-              </p>
-            </div>
-            <hr />
-            {/* Mani container */}
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                {/* Customer avatar */}
-                <div className="avatar">
-                  <div className="w-10 rounded-full">
-                    <img src={customer_profile.photo} alt="" />
+              <img src={image} alt="Shoes" />
+            </figure>
+            <div className="card-body space-y-2">
+              <h2 className="card-title">{estate_title}</h2>
+              <div className="flex items-center">
+                <p className="flex items-center gap-2 text-base">
+                  <GrStatusInfo />
+                  For {status}
+                </p>
+                <p className="flex items-center gap-2 text-base">
+                  <BiLocationPlus />
+                  {location}
+                </p>
+                <p className="flex items-center gap-2 text-base">
+                  <BiArea />
+                  {area}
+                </p>
+              </div>
+              <hr />
+              {/* Mani container */}
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  {/* Customer avatar */}
+                  <div className="avatar">
+                    <div className="w-10 rounded-full">
+                      <img src={customer_profile.photo} alt="" />
+                    </div>
                   </div>
-                </div>
-                {/* Customer name */}
-                <div>
+                  {/* Customer name */}
                   <div>
-                    <p className="text-base">{customer_profile.name}</p>
+                    <div>
+                      <p className="text-base">{customer_profile.name}</p>
+                    </div>
                   </div>
                 </div>
+                {/* price */}
+                <div>
+                  <p className="text-base font-medium">${price}</p>
+                </div>
               </div>
-              {/* price */}
+              {/* Button */}
               <div>
-                <p className="text-base font-medium">${price}</p>
+                <Link to={`/estateDetails/${id}`}>
+                  <button className="btn rounded-full px-8 text-white bg-[#1563DF]">
+                    View Property
+                  </button>
+                </Link>
               </div>
-            </div>
-            {/* Button */}
-            <div>
-              <Link to={`/estateDetails/${id}`}>
-                <button className="btn rounded-full px-8 text-white bg-[#1563DF]">
-                  View Property
-                </button>
-              </Link>
             </div>
           </div>
         </div>
-      </div>
+      </AnimatedComponent>
     </>
   );
 };
